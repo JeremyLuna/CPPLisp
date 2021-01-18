a722027757c79981066c05c62d11865f6b944b84
Jeremy Luna <jeremylunaalpha@gmail.com>
2021-01-18 14:13:50 -0600

    splitting ( doesnt actually work

---
+++ cpplisp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+file_name = sys.argv[1]
+
+def cpplisp_str_to_cpp_str(cpplisp_str):
+
+
+cpplisp_str = open(file_name, 'r').read()
+
+cpp_str = cpplisp_str_to_cpp_str(cpplisp_str)
+
+# write cpp_str to file
+# compile file
+# run file
